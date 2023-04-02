86d686ceaa08abfdaf5df842a96a1c612042f678
fatimafneish <112481762+fatima-fneish@users.noreply.github.com>
2023-04-02 16:30:20 +0300

    updated app.py

--- app.py
+++ app.py
@@ -1,40 +1,23 @@
-import os
-import logging
-
-from flask import Flask, request, jsonify
-
+from flask import Flask, render_template, request
 from model.model import EngFrenTranslator
 
-app = Flask(__name__)  
+app = Flask(__name__, template_folder='templates')
 
 # define model path
 model_path = './model/my_model.h5'
 eng_tokenizer_path='./model/eng_tokenizer.json'
 frn_tokenizer_path='./model/frn_tokenizer.json'
 # create instance
-model = EngFrenTranslator(model_path,eng_tokenizer_path,frn_tokenizer_path)
-logging.basicConfig(level=logging.INFO)
+model = EngFrenTranslator(model_path, eng_tokenizer_path, frn_tokenizer_path)
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    """Provide simple health check route."""
-    return "Hello world!"
+    if request.method == 'POST':
+        input_text = request.form['input_text']
+        output_text = model.predict(input_text)
+        return render_template('index.html', output_text=output_text)
+    else:
+        return render_template('index.html')
 
-
-@app.route("/v1/predict", methods=["GET", "POST"])
-def predict():
-    """Provide main prediction API route. Responds to both GET and POST requests."""
-    logging.info("Predict request received!")
-    input = request.args.get("input")
-    prediction = model.predict(input)
-    
-    logging.info("prediction from model= {}".format(prediction))
-    return jsonify({"predicted_class": str(prediction)})
-
-def main():
-    """Run the Flask app."""
-    app.run(host="0.0.0.0", port=8000, debug=False) 
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run()
